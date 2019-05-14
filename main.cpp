6c7a78ddea9a7ad40c7ece491eeb931989a8046b
Alexander Myasnikov <myasnikov.alexander.s@gmail.com>
2019-05-14 22:21:45 +0300

    refactoring

--- main.cpp
+++ main.cpp
@@ -16,21 +16,41 @@ using namespace std::string_literals;
 
 namespace lisp_interpreter {
 
+  // syntax
+  // list:  LIST head tail | LIST Undefined Undefined
+  // head:  list | atom
+  // tail:  list
+  // atom:  keyword: digit+ | digit+.digit+ | char[char|digit|-]*
+
+  // syntax. example
+  // () -> LIST Undefined Undefined
+  // (1) -> LIST 1 (LIST Undefined Undefined)
+  // (1 2) -> LIST 1 (LIST 2 (LIST Undefined Undefined))
+  // (1 (2)) -> LIST 1 (LIST (LIST 2 (LIST Undefined Undefined)) (LIST Undefined Undefined))
+
   struct object_t {
+    using sptr_t = std::shared_ptr<object_t>;
     virtual ~object_t() { }
   };
 
-  using object_sptr_t      = std::shared_ptr<object_t>;
+  struct object_list_t : object_t {
+    using sptr_t = std::shared_ptr<object_list_t>;
 
-  // struct object_null_t     : object_t { };
-  struct object_int_t      : object_t { int value; };
-  struct object_double_t   : object_t { double value; };
-  struct object_string_t   : object_t { std::string value; };
-  struct object_variable_t : object_t { std::string name; };
-  struct object_list_t     : object_t { object_sptr_t head; object_sptr_t tail; };
-  struct object_keyword_t  : object_t { int id; };
+    object_t::sptr_t        head;
+    object_list_t::sptr_t   tail;
 
-  using object_list_sptr_t = std::shared_ptr<object_list_t>;
+    object_list_t(object_t::sptr_t head, object_list_t::sptr_t tail) : head(head), tail(tail) { }
+    object_list_t(object_t::sptr_t head) : head(head), tail(nullptr) { }
+    object_list_t() : head(nullptr), tail(nullptr) { }
+  };
+
+  struct object_atom_t : object_t {
+    using sptr_t = std::shared_ptr<object_atom_t>;
+
+    std::string name;
+
+    object_atom_t(const std::string& name) : name(name) { }
+  };
 
   std::vector<std::string> keywords = {
     "+",
@@ -40,34 +60,68 @@ namespace lisp_interpreter {
   };
 
 
-  object_sptr_t car(const object_sptr_t& object) { // car (список) -> S-выражение.
+  // car (список) -> S-выражение.
+  object_t::sptr_t car(const object_t::sptr_t& object) {
     auto object_list = std::dynamic_pointer_cast<object_list_t>(object);
-    if (!object_list) throw std::runtime_error("expected list");
+    if (!object_list) throw std::runtime_error("car: expected list");
+    if (!object_list->head) throw std::runtime_error("car: head not exist");
     return object_list->head;
   }
 
-  object_sptr_t cdr(const object_sptr_t& object) { // cdr (список) -> список
+  // cdr (список) -> список
+  object_t::sptr_t cdr(const object_t::sptr_t& object) {
     auto object_list = std::dynamic_pointer_cast<object_list_t>(object);
-    if (!object_list) throw std::runtime_error("expected list");
+    if (!object_list) throw std::runtime_error("cdr: expected list");
+    if (!object_list->head) throw std::runtime_error("cdr: tail not exist");
     return object_list->tail;
   }
 
-  object_sptr_t cons(const object_sptr_t& head, const object_sptr_t& tail) { // cons (s-выражение, список) -> список
+  // cons (s-выражение, список) -> список
+  object_t::sptr_t cons(const object_t::sptr_t& head, const object_t::sptr_t& tail) {
     auto tail_list = std::dynamic_pointer_cast<object_list_t>(tail);
-    if (!tail_list) throw std::runtime_error("expected list");
+    if (!tail_list) throw std::runtime_error("cons: expected list");
 
-    auto object_list = std::make_shared<object_list_t>();
-    object_list->head = head;
-    object_list->tail = tail;
+    auto object_list = std::make_shared<object_list_t>(head, tail_list);
     return object_list;
   }
 
 
-  struct parser_t {
-    std::stack<object_sptr_t> stack;
-    object_sptr_t object = nullptr;
 
-    object_sptr_t parse(const std::string& str) {
+  struct shower_t {
+    void show(object_t::sptr_t object) {
+      std::cout << "(";
+      show_list(object); // XXX
+      std::cout << ")";
+    }
+    void show_list(object_t::sptr_t object) {
+      if (!object) {
+        return;
+      }
+
+      if (auto object_list = std::dynamic_pointer_cast<object_list_t>(object); object_list) {
+        if (auto object_list_head = std::dynamic_pointer_cast<object_list_t>(object_list->head); object_list_head) {
+          std::cout << "(";
+          show_list(object_list->head);
+          std::cout << ")";
+        } else {
+          show_list(object_list->head);
+        }
+        show_list(object_list->tail);
+      } else if (auto object_atom = std::dynamic_pointer_cast<object_atom_t>(object); object_atom) {
+        std::cout << object_atom->name << " ";
+      } else {
+          throw std::runtime_error("show_list: unknown type");
+      }
+    }
+  };
+
+
+
+  struct parser_t {
+    std::stack<object_t::sptr_t> stack;
+    object_t::sptr_t object = nullptr;
+
+    object_t::sptr_t parse(const std::string& str) {
       const char *it = str.c_str();
       const char *ite = str.c_str() + str.size();
       int value;
@@ -77,14 +131,15 @@ namespace lisp_interpreter {
           std::cout << "push list" << std::endl;
           stack.emplace(std::make_shared<object_list_t>());
           if (stack.size() == 1) {
-            if (!object)
-              object = stack.top();
-            else
-              object = cons(object, stack.top());;
+            //if (!object) // XXX
+              //object = stack.top();
+            //else
+              //object = cons(object, stack.top());;
           }
         } else if (c == ')') {
           if (stack.empty()) throw std::runtime_error("unexpected ')'");
           std::cout << "pop list" << std::endl;
+          { std::cout << "  XXX list: "; shower_t().show(stack.top()); std::cout << std::endl; }
           stack.pop();
         } else if (std::isspace(c)) {
           ;
@@ -95,25 +150,18 @@ namespace lisp_interpreter {
                   itf != keywords.end()) {
           std::cout << "keyword: '" << *itf << "'" << std::endl;
           it += itf->size() - 1;
-        } else if (auto [p, ec] = std::from_chars(it, ite, value); ec == std::errc()) {
-          auto object_int = std::make_shared<object_int_t>();
-          object_int->value = value;
-          std::cout << "digit: '" << value << "'" << std::endl;
-          it += (p - it) - 1;
-          if (!object)
-            object = std::make_shared<object_list_t>();
-          object = cons(object_int, object); // XXX
-        } else if (std::isalpha(c)) {
-          auto object_variable = std::make_shared<object_variable_t>();
-          auto itf = std::find_if(it, ite, [](char c) { return !isalnum(c) && c != '-'; });
-          object_variable->name = std::string(it, itf);
-          std::cout << "variable: '" << object_variable->name << "'" << std::endl;
+        } else if (std::isalnum(c)) {
+          auto itf = std::find_if(it, ite, [](char c) { return !isalnum(c); });
+          auto object_atom = std::make_shared<object_atom_t>(std::string(it, itf));
+          std::cout << "name: '" << object_atom->name << "'" << std::endl;
+          // auto object_n = cons(stack.top(), object_atom); // XXX
+          // stack.pop();
+          // stack.emplace(object_n);
           it += (itf - it) - 1;
         } else if (c == '"') {
-          auto object_string = std::make_shared<object_string_t>();
           auto itf = std::find(it + 1, ite, '"');
-          object_string->value = std::string(it + 1, itf);
-          std::cout << "string: '" << object_string->value << "'" << std::endl;
+          auto object_atom = std::make_shared<object_atom_t>(std::string(it + 1, itf));
+          std::cout << "name: '" << object_atom->name << "'" << std::endl;
           it += (itf - it);
         } else {
           throw std::runtime_error("parse: unknown char: '"s + c + "'");
@@ -125,69 +173,55 @@ namespace lisp_interpreter {
       return object;
     }
   };
-
-  struct shower_t {
-    void show(object_sptr_t object) {
-      if (!object) return;
-
-      if (auto object_list = std::dynamic_pointer_cast<object_list_t>(object); object_list) {
-        // std::cout << "(" << std::endl;
-        show(object_list->head);
-        show(object_list->tail);
-        // std::cout << ")" << std::endl;
-      } else if (auto object_int = std::dynamic_pointer_cast<object_int_t>(object); object_int) {
-        std::cout << object_int->value << std::endl;
-      } else if (auto object_double = std::dynamic_pointer_cast<object_double_t>(object); object_double) {
-        std::cout << object_double->value << std::endl;
-      } else if (auto object_string = std::dynamic_pointer_cast<object_string_t>(object); object_string) {
-        std::cout << "\"" << object_string->value << "\"" << std::endl;
-      } else if (auto object_variable = std::dynamic_pointer_cast<object_variable_t>(object); object_variable) {
-        std::cout << object_variable->name << std::endl;
-      } else if (auto object_keyword = std::dynamic_pointer_cast<object_keyword_t>(object); object_keyword) {
-        std::cout << keywords.at(object_keyword->id) << std::endl;
-      } else {
-          throw std::runtime_error("show: unknown type");
-      }
-    }
-  };
-
-
-
-  /*inline static const std::vector<object_sptr_t> objects {
-    // std::make_shared<object_null_t>(),
-    // std::make_shared<object_double_t>(),
-    // std::make_shared<object_string_t>(),
-    std::make_shared<object_list_t>(),
-  };*/
-
-  /*static std::string_view parse(const std::string_view& str) {
-    auto it = str.cbegin();
-    auto ite = str.cend();
-    auto s = str;
-    while (it != ite) {
-      for (const auto& o : objects) {
-        auto r = o->parse(str);
-        if (r.second) {
-          it = r.first.cbegin() + 1;
-          break;
-        }
-      }
-
-
-      ++it;
-    }
-    return str;
-  }*/ 
 }
 
 int main() {
   using namespace lisp_interpreter;
 
-  std::string str = R"LISP(1 29 abc (  + 1   "2" 34) 56 78 )LISP";
-  std::cout << "input: " << str << std::endl;
+  {
+    auto l_ex1 = std::make_shared<object_list_t>(
+        std::make_shared<object_atom_t>("*"),
+        std::make_shared<object_list_t>(
+          std::make_shared<object_atom_t>("2"),
+          std::make_shared<object_list_t>(
+            std::make_shared<object_list_t>(
+              std::make_shared<object_atom_t>("+"),
+              std::make_shared<object_list_t>(
+                std::make_shared<object_atom_t>("3"),
+                std::make_shared<object_list_t>(
+                  std::make_shared<object_atom_t>("4")
+                  )
+                )
+              )
+            )
+          )
+        );
 
-  auto object = parser_t().parse(str);
-  shower_t().show(object);
+    auto l_ex2 = std::make_shared<object_atom_t>("1");
+
+    auto l_ex3 = std::make_shared<object_list_t>(
+        std::make_shared<object_atom_t>("1")
+        );
+
+    auto l_ex4 = std::make_shared<object_list_t>(
+        std::make_shared<object_list_t>(
+          std::make_shared<object_atom_t>("2")
+          )
+        );
+
+    // shower_t().show(l_ex1);
+    // std::cout << std::endl;
+  }
+
+  {
+    std::string str = R"LISP((1 29 abc (  + 1   "2" 34) 56 78 ))LISP";
+    std::cout << "input: " << str << std::endl;
+
+    auto object = parser_t().parse(str);
+
+    shower_t().show(object);
+    std::cout << std::endl;
+  }
 
   std::cout << "end." << std::endl;
 
