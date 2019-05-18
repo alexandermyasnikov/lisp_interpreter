b0098b285296c30c409c472519cdc5b53a23ee77
Alexander Myasnikov <myasnikov.alexander.s@gmail.com>
2019-05-18 22:48:51 +0300

    refactoring

--- main.cpp
+++ main.cpp
@@ -4,6 +4,7 @@
 #include <vector>
 #include <stack>
 #include <variant>
+#include <optional>
 #include <algorithm>
 
 #define PR        std::cout << __FUNCTION__ << '\t' << __LINE__ << std::endl
@@ -29,19 +30,28 @@ namespace lisp_interpreter {
   // (1 (2)) -> LIST 1 (LIST (LIST 2 (LIST Undefined Undefined)) (LIST Undefined Undefined))
 
   struct object_undefined_t;
+  struct object_error_t;
   struct object_atom_t;
   struct object_list_t;
 
   using object_undefined_sptr_t = std::shared_ptr<const object_undefined_t>;
+  using object_error_sptr_t = std::shared_ptr<const object_error_t>;
   using object_atom_sptr_t = std::shared_ptr<const object_atom_t>;
   using object_list_sptr_t = std::shared_ptr<const object_list_t>;
 
   using object_t = std::variant<
     object_undefined_sptr_t,
+    object_error_sptr_t,
     object_atom_sptr_t,
     object_list_sptr_t>;
 
   struct object_undefined_t { };
+
+  struct object_error_t {
+    std::string msg;
+
+    object_error_t(const std::string& msg) : msg(msg) { }
+  };
 
   struct object_list_t {
     object_t head;
@@ -68,21 +78,21 @@ namespace lisp_interpreter {
 
   // I N T E R F A C E
 
-  object_t new_atom(const std::string& name) {
-    return std::make_shared<object_atom_t>(name);
-  }
-
-  object_t new_undefined() {
+  object_t undefined() { // private
     static auto undefined = std::make_shared<object_undefined_t>();
     return undefined;
   }
 
-  object_t new_list() {
-    return std::make_shared<object_list_t>(new_undefined(), new_undefined());
+  object_t error(const std::string& msg) {
+    return std::make_shared<object_error_t>(msg);
   }
 
-  bool is_undefined(object_t object) {
-    return std::get_if<object_undefined_sptr_t>(&object);
+  object_t atom(const std::string& name) {
+    return std::make_shared<object_atom_t>(name);
+  }
+
+  object_t list() {
+    return std::make_shared<object_list_t>(undefined(), undefined());
   }
 
   bool is_atom(object_t object) {
@@ -93,39 +103,90 @@ namespace lisp_interpreter {
     return std::get_if<object_list_sptr_t>(&object);
   }
 
+  bool is_undefined(object_t object) {
+    return std::get_if<object_undefined_sptr_t>(&object);
+  }
+
+  bool is_error(object_t object) {
+    return std::get_if<object_error_sptr_t>(&object);
+  }
+
   object_t car(object_t object) {
-    if (!is_list(object)) return new_undefined();
+    if (is_error(object)) return object;
+    if (!is_list(object)) return error("car: object is not list");
     auto list = std::get<object_list_sptr_t>(object);
+    if (is_undefined(list->head)) return error("car: object->head is undefined");
     return list->head;
   }
 
   object_t cdr(object_t object) {
-    if (!is_list(object)) return new_undefined();
+    if (is_error(object)) return object;
+    if (!is_list(object)) return error("cdr: expected list");
     auto list = std::get<object_list_sptr_t>(object);
+    if (is_undefined(list->tail)) return error("cdr: object->tail is undefined");
     return list->tail;
   }
 
   object_t cons(object_t head, object_t tail) {
+    if (is_error(head)) return head;
+    if (is_error(tail)) return tail;
+    if (!is_list(tail)) return error("cons: tail is not list");
+    if (is_error(car(tail))) {
+      if (is_error(cdr(tail))) {
+        return std::make_shared<object_list_t>(head, undefined());
+      } else {
+        return cons(head, cdr(tail));
+      }
+    }
     return std::make_shared<object_list_t>(head, tail);
+  }
+
+  // L I B R A R Y
+
+  object_t reverse(object_t object) {
+    if (!is_list(object))
+      return object;
+    auto list_r = list();
+    while (is_list(object)) {
+      list_r = cons(reverse(car(object)), list_r);
+      object = cdr(object);
+    }
+    return list_r;
   }
 
 
 
   struct shower_t {
     std::string show(object_t object) {
-      return "(" + show_list(object) + ") ";
+      if (is_error(object))
+        return std::get<object_error_sptr_t>(object)->msg + " ";
+      return is_list(object) ? "( " + show_list(object) + ") " : show_list(object);
     }
+
     std::string show_list(object_t object) {
       std::string str;
-      std::get_if<object_list_sptr_t>(&object);
-      if (is_undefined(object)) {
-        ;
-      } else if (is_atom(object)) {
+      if (is_atom(object)) {
         str += std::get<object_atom_sptr_t>(object)->name + " ";
       } else if (is_list(object)) {
         auto head = car(object);
-        str += is_list(head) ? show(head) : show_list(head);
+        str += is_list(head) ? "( " + show_list(head) + ") " : show_list(head);
         str += show_list(cdr(object));
+      }
+      return str;
+    }
+
+    std::string show_n(object_t object) {
+      std::string str;
+      if (is_undefined(object)) {
+        str += "U ";
+      } else if (is_atom(object)) {
+        str += std::get<object_atom_sptr_t>(object)->name + " ";
+      } else if (is_list(object)) {
+        auto head = is_error(car(object)) ? undefined() : car(object); // XXX сомнительно
+        auto tail = is_error(cdr(object)) ? undefined() : cdr(object);
+        str += "LIST ( " + show_n(head) + ") ( " + show_n(tail) + ") ";
+      } else if (is_error(object)) {
+        str += "E ";
       }
       return str;
     }
@@ -135,7 +196,7 @@ namespace lisp_interpreter {
 
   struct parser_t {
     std::stack<object_t> stack;
-    object_t object = new_undefined();
+    object_t object = list();
 
     object_t parse(const std::string& str) {
       const char *it = str.c_str();
@@ -144,22 +205,10 @@ namespace lisp_interpreter {
         char c = *it;
         if (c == '(') {
           // std::cout << "push list" << std::endl;
-          stack.emplace(new_list());
+          stack.emplace(list());
         } else if (c == ')') {
-          if (stack.empty()) throw std::runtime_error("unexpected ')'");
           // std::cout << "pop list" << std::endl;
-
-          auto list = stack.top();
-          auto list_r = new_list();
-          while (is_list(list)) {
-            list_r = cons(car(list), list_r); // reverse
-            list = cdr(list);
-          }
-          stack.top().swap(list_r);
-
-          if (stack.size() == 1) { // save object
-            object = cons(object, stack.top());
-          }
+          if (stack.empty()) throw std::runtime_error("unexpected ')'");
 
           auto tmp = stack.top();
           stack.pop();
@@ -167,10 +216,9 @@ namespace lisp_interpreter {
           if (!stack.empty()) {
             auto object_n = cons(tmp, stack.top());
             stack.top().swap(object_n);
+          } else if (stack.empty()) { // save object
+            object = cons(tmp, object);
           }
-
-        } else if (std::isspace(c)) {
-          ;
         } else if (auto itf = std::find_if(keywords.begin(), keywords.end(),
               [&](const std::string& keyword) {
                   return 0 == keyword.compare(0, keyword.size(), it,
@@ -178,32 +226,43 @@ namespace lisp_interpreter {
                   itf != keywords.end()) {
           auto object_atom = std::make_shared<object_atom_t>(*itf);
           // std::cout << "keyword: '" << object_atom->name << "'" << std::endl;
-          if (stack.empty()) throw std::runtime_error("parse: expected list");
-          auto object_n = cons(object_atom, stack.top());
-          stack.top().swap(object_n);
+          if (stack.empty()) {
+            // object = cons(object_atom, object);
+          } else {
+            auto object_n = cons(object_atom, stack.top());
+            stack.top().swap(object_n);
+          }
           it += itf->size() - 1;
         } else if (std::isalnum(c)) {
           auto itf = std::find_if(it, ite, [](char c) { return !isalnum(c); });
           auto object_atom = std::make_shared<object_atom_t>(std::string(it, itf));
           // std::cout << "variable: '" << object_atom->name << "'" << std::endl;
-          if (stack.empty()) throw std::runtime_error("parse: expected list");
-          auto object_n = cons(object_atom, stack.top());
-          stack.top().swap(object_n);
+          if (stack.empty()) {
+            // XXX
+            // object = cons(object_atom, object);
+            // *object = (object) ? cons(object_atom, *object) : cons(object_atom, list());
+          } else {
+            auto object_n = cons(object_atom, stack.top());
+            stack.top().swap(object_n);
+          }
           it += (itf - it) - 1;
         } else if (c == '"') {
           auto itf = std::find(it + 1, ite, '"');
           if (itf == ite) throw std::runtime_error("parse: expected '\"'");
           auto object_atom = std::make_shared<object_atom_t>(std::string(it, itf + 1));
           // std::cout << "string: '" << object_atom->name << "'" << std::endl;
-          if (stack.empty()) throw std::runtime_error("parse: expected list");
-          auto object_n = cons(object_atom, stack.top());
-          stack.top().swap(object_n);
+          if (stack.empty()) {
+            // object = cons(object_atom, object);
+          } else {
+            auto object_n = cons(object_atom, stack.top());
+            stack.top().swap(object_n);
+          }
           it += (itf - it);
         }
         ++it;
       }
       if (!stack.empty()) throw std::runtime_error("expected ')'");
-      return object;
+      return reverse(object);
     }
   };
 }
@@ -216,46 +275,68 @@ int main() {
   // T E S T
 
   {
-    auto l = new_list();
-    assert(is_list(l));
-    assert(is_undefined(car(l)));
-    assert(is_undefined(cdr(l)));
+    auto a = atom("a");
+    assert(is_atom(a));
+    assert(shower_t().show(a) == R"LISP(a )LISP");
+    assert(shower_t().show_n(a) == R"LISP(a )LISP");
   }
+
   {
-    auto u = new_undefined();
-    auto l = cons(new_atom("1"), u);
-    assert(is_list(l));
-    assert(!is_undefined(car(l)));
-    assert(is_undefined(cdr(l)));
+    auto l = list();
+    assert(is_error(car(l)));
+    assert(is_error(cdr(l)));
+    assert(shower_t().show_n(l) == R"LISP(LIST ( U ) ( U ) )LISP");
+    assert(shower_t().show(l) == R"LISP(( ) )LISP");
   }
+
+  {
+    auto l = cons(atom("a"), list());
+    assert(is_atom(car(l)));
+    assert(is_error(cdr(l)));
+    assert(shower_t().show_n(l) == R"LISP(LIST ( a ) ( U ) )LISP");
+    assert(shower_t().show(l) == R"LISP(( a ) )LISP");
+  }
+
+  {
+    auto l = cons(atom("a"), cons(atom("b"), list()));
+    assert(is_atom(car(l)));
+    assert(is_list(cdr(l)));
+    assert(shower_t().show_n(l) == R"LISP(LIST ( a ) ( LIST ( b ) ( U ) ) )LISP");
+    assert(shower_t().show(l) == R"LISP(( a b ) )LISP");
+  }
+
+  {
+    auto l = cons(list(), cons(atom("b"), list()));
+    assert(is_list(car(l)));
+    assert(is_list(cdr(l)));
+    assert(shower_t().show_n(l) == R"LISP(LIST ( LIST ( U ) ( U ) ) ( LIST ( b ) ( U ) ) )LISP");
+    assert(shower_t().show(l) == R"LISP(( ( ) b ) )LISP");
+  }
+
   {
     auto expr = cons(
-        new_atom("*"),
+        atom("*"),
         cons(
-          new_atom("2"),
+          atom("2"),
           cons(
             cons(
-              new_atom("+"),
+              atom("+"),
               cons(
-                new_atom("3"),
+                atom("3"),
                 cons(
-                  new_atom("4"),
-                  new_undefined()))),
-            new_undefined())));
+                  atom("4"),
+                  list()))),
+            list())));
 
-    assert(shower_t().show(expr) == "(* 2 (+ 3 4 ) ) ");
-  }
-  {
-    std::string str = R"LISP((1 29 abc (  + 1   "2" 34 ("a" "b" "c")) 56 78 ))LISP";
-    auto object = parser_t().parse(str);
-    assert(shower_t().show(object) == R"LISP((1 29 abc (+ 1 "2" 34 ("a" "b" "c" ) ) 56 78 ) )LISP");
+    assert(shower_t().show(expr) == "( * 2 ( + 3 4 ) ) ");
   }
 
   {
-    std::string str = R"LISP((1 29 abc (  + 1   "2" 34 ("a" "b" "c")) 56 78 ))LISP";
-    std::cout << "input: " << str << std::endl;
+    std::string str = R"LISP((1 2 (3)) (4) (5))LISP";
+    std::cout << "input: '" << str << "'" << std::endl;
     auto object = parser_t().parse(str);
-    std::cout << "output: " << shower_t().show(object) << std::endl;
+    std::cout << "output: '" << shower_t().show(object) << "'" << std::endl;
+    std::cout << "output: '" << shower_t().show_n(object) << "'" << std::endl;
   }
 
   return 0;
