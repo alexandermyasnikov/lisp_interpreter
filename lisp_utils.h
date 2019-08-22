413ebbc2b124fd0206fade87b76c9e7888694563
Alexander Myasnikov <myasnikov.alexander.s@gmail.com>
2019-08-22 18:54:17 +0300

    refactoring

--- lisp_utils.h
+++ lisp_utils.h
@@ -80,37 +80,51 @@ namespace lisp_utils {
 
   struct lisp_compiler_t {
 
-    struct token_empty_t   { };
-    struct token_lp_t      { };
-    struct token_rp_t      { };
-    struct token_bool_t    { bool        value; };
-    struct token_integer_t { int64_t     value; };
-    struct token_double_t  { double      value; };
-    struct token_string_t  { std::string value; };
-    struct token_ident_t   { std::string value; };
-    struct token_list_t;
-
-    using token_t = std::variant<
-      token_empty_t,
-      token_lp_t,
-      token_rp_t,
-      std::shared_ptr<token_bool_t>,
-      std::shared_ptr<token_integer_t>,
-      std::shared_ptr<token_double_t>,
-      std::shared_ptr<token_string_t>,
-      std::shared_ptr<token_ident_t>,
-      std::shared_ptr<token_list_t>>;
-
-    using tokens_t = std::list<token_t>;
-    using ast_t = std::shared_ptr<token_t>; // abstract syntax tree
-
-    struct token_list_t {
-      std::list<std::shared_ptr<token_t>> list;
-    };
-
     // program       : stmt*
     // stmt          : BOOL | INT | DOUBLE | STRING | IDENT | list
     // list          : LP stmt* RP
+
+    struct lexeme_empty_t   { };
+    struct lexeme_lp_t      { };
+    struct lexeme_rp_t      { };
+    struct lexeme_bool_t    { bool        value; };
+    struct lexeme_integer_t { int64_t     value; };
+    struct lexeme_double_t  { double      value; };
+    struct lexeme_string_t  { std::string value; };
+    struct lexeme_ident_t   { std::string value; };
+
+    using lexeme_t = std::variant<
+      lexeme_empty_t,
+      lexeme_lp_t,
+      lexeme_rp_t,
+      lexeme_bool_t,
+      lexeme_integer_t,
+      lexeme_double_t,
+      lexeme_string_t,
+      lexeme_ident_t>;
+
+    struct lexeme_list_t;
+
+    using syntax_tree_t = std::variant<
+      lexeme_t,
+      std::shared_ptr<lexeme_list_t>>;
+
+    struct lexeme_list_t {
+      std::vector<syntax_tree_t> list;
+    };
+
+    using lexemes_t = std::list<lexeme_t>;
+
+
+
+    struct semantic_def_stmt_t {
+    };
+
+    struct semantic_program_t {
+      std::vector<semantic_def_stmt_t> def_stmt;
+    };
+
+    using semantic_tree_t = semantic_program_t;
 
     // program       : def_stmt*
     // def_stmt      : LP __def IDENT lambda_stmt RP
@@ -121,27 +135,37 @@ namespace lisp_utils {
     // expr          : atom | fun_stmt | if_stmt
     // atom          : BOOL | INT | DOUBLE | STRING | IDENT | lambda_stmt
 
-    static std::string show_token(const token_t& token) {
+
+
+    static std::string show_lexeme(const lexeme_t& lexeme) {
       std::string str;
       std::visit(overloaded {
-        [&str] (token_empty_t)                              { str = "(empty)"; },
-        [&str] (token_lp_t)                                 { str = "("; },
-        [&str] (token_rp_t)                                 { str = ")"; },
-        [&str] (std::shared_ptr<token_bool_t>      value)   { str = value->value ? "true" : "false"; },
-        [&str] (std::shared_ptr<token_integer_t>   value)   { str = std::to_string(value->value); },
-        [&str] (std::shared_ptr<token_double_t>    value)   { str = std::to_string(value->value); },
-        [&str] (std::shared_ptr<token_string_t>    value)   { str = "\"" + value->value + "\""; },
-        [&str] (std::shared_ptr<token_ident_t>     value)   { str = value->value; },
-        [&str] (std::shared_ptr<token_list_t>      value)   { str = "( "; for (const auto& t : value->list) str += show_token(*t) + " "; str += ")"; },
-        [&str] (auto)                                       { str = "(unknown)"; },
-      }, token);
+        [&str] (lexeme_empty_t)                { str = "(empty)"; },
+        [&str] (lexeme_lp_t)                   { str = "("; },
+        [&str] (lexeme_rp_t)                   { str = ")"; },
+        [&str] (const lexeme_bool_t    &value) { str = value.value ? "true" : "false"; },
+        [&str] (const lexeme_integer_t &value) { str = std::to_string(value.value); },
+        [&str] (const lexeme_double_t  &value) { str = std::to_string(value.value); },
+        [&str] (const lexeme_string_t  &value) { str = "\"" + value.value + "\""; },
+        [&str] (const lexeme_ident_t   &value) { str = value.value; },
+        [&str] (auto)                          { str = "(unknown)"; },
+      }, lexeme);
       return str;
     }
 
-    std::string show_ast(ast_t ast) {
-      token_t token = *ast;
-      std::string ret = show_token(token);
-      return ret;
+    static std::string show_syntax_tree(const syntax_tree_t& syntax_tree) {
+      std::string str;
+      std::visit(overloaded {
+        [&str] (std::shared_ptr<lexeme_list_t> value) {
+          str += "( ";
+          for (const auto& v : value->list)
+            str += show_syntax_tree(v) + " ";
+          str += ")";
+        },
+        [&str] (const lexeme_t& value) { str = show_lexeme(value); },
+        [&str] (auto)                  { str = "(unknown)"; },
+      }, syntax_tree);
+      return str;
     }
 
 
@@ -150,7 +174,7 @@ namespace lisp_utils {
 
       struct rule_t {
         std::regex   regex;
-        std::function<token_t (const std::string&)> get_token;
+        std::function<lexeme_t (const std::string&)> get_lexeme;
       };
 
       using rules_t = std::list<rule_t>;
@@ -158,46 +182,45 @@ namespace lisp_utils {
       static inline rules_t rules = {
         {
           std::regex("\\s+|;.*?\n"),
-          [](const std::string&) { return token_empty_t{}; }
+          [](const std::string&) { return lexeme_empty_t{}; }
         }, {
           std::regex("\\("),
-          [](const std::string&) { return token_lp_t{}; }
+          [](const std::string&) { return lexeme_lp_t{}; }
         }, {
           std::regex("\\)"),
-          [](const std::string&) { return token_rp_t{}; }
+          [](const std::string&) { return lexeme_rp_t{}; }
         }, {
           std::regex("true", std::regex_constants::icase),
-          [](const std::string&) { return std::make_shared<token_bool_t>(token_bool_t{true}); }
+          [](const std::string&) { return lexeme_bool_t{true}; }
         }, {
           std::regex("false", std::regex_constants::icase),
-          [](const std::string&) { return std::make_shared<token_bool_t>(token_bool_t{false}); }
+          [](const std::string&) { return lexeme_bool_t{false}; }
         }, {
           std::regex("[-+]?((\\d+\\.\\d*)|(\\d*\\.\\d+))"),
-          [](const std::string& str) { return std::make_shared<token_double_t>(token_double_t{std::stod(str)}); }
+          [](const std::string& str) { return lexeme_double_t{std::stod(str)}; }
         }, {
           std::regex("\".*?\""),
-          [](const std::string& str) { return std::make_shared<token_string_t>(
-              token_string_t{str.substr(1, str.size() - 2)}); }
+          [](const std::string& str) { return lexeme_string_t{str.substr(1, str.size() - 2)}; }
         }, {
           std::regex("[-+]?\\d+"),
-          [](const std::string& str) { return std::make_shared<token_integer_t>(token_integer_t{std::stol(str)}); }
+          [](const std::string& str) { return lexeme_integer_t{std::stol(str)}; }
         }, {
           std::regex("[\\w!#$%&*+-./:<=>?@_]+"),
-          [](const std::string& str) { return std::make_shared<token_ident_t>(token_ident_t{str}); }
+          [](const std::string& str) { return lexeme_ident_t{str}; }
         }
       };
 
-      tokens_t parse(const std::string& str) {
+      lexemes_t parse(const std::string& str) {
         DEBUG_LOGGER_TRACE_ULISP;
-        tokens_t tokens;
+        lexemes_t lexemes;
         std::string s = str;
         std::smatch m;
         while (!s.empty()) {
           for (const auto& rule : rules) {
             if (std::regex_search(s, m, rule.regex, std::regex_constants::match_continuous)) {
-              token_t token = rule.get_token(m.str());
-              if (!std::get_if<token_empty_t>(&token))
-                tokens.push_back(token);
+              lexeme_t lexeme = rule.get_lexeme(m.str());
+              if (!std::get_if<lexeme_empty_t>(&lexeme))
+                lexemes.push_back(lexeme);
               s = m.suffix().str();
               break;
             }
@@ -210,83 +233,90 @@ namespace lisp_utils {
             s.erase(s.begin());
           }
         }
-        return tokens;
+        return lexemes;
       }
     };
 
     struct syntax_analyzer {
 
-      ast_t parse(tokens_t tokens) {
+      syntax_tree_t parse(const lexemes_t& lexemes) {
         DEBUG_LOGGER_TRACE_ULISP;
-        std::stack<std::shared_ptr<token_list_t>> stack;
-        stack.push(std::make_shared<token_list_t>());
-        for (const auto& token : tokens) {
-          if (std::get_if<token_lp_t>(&token)) {
-            stack.push(std::make_shared<token_list_t>());
-          } else if (std::get_if<token_rp_t>(&token)) {
-            auto top = std::make_shared<token_t>(stack.top());
+        std::stack<lexeme_list_t> stack;
+        stack.push(lexeme_list_t{});
+        for (const auto& lexeme : lexemes) {
+          if (std::get_if<lexeme_lp_t>(&lexeme)) {
+            stack.push(lexeme_list_t{});
+          } else if (std::get_if<lexeme_rp_t>(&lexeme)) {
+            auto top = stack.top();
             stack.pop();
-            stack.top()->list.push_back(top);
+            stack.top().list.push_back(std::make_shared<lexeme_list_t>(std::move(top)));
           } else {
-            stack.top()->list.push_back(std::make_shared<token_t>(token));
+            stack.top().list.push_back(lexeme);
           }
         }
         if (stack.size() != 1) throw std::runtime_error("syntax_analyzer: parse error");
-        auto ret = std::make_shared<token_t>(stack.top());
-        return ret;
+        return std::make_shared<lexeme_list_t>(std::move(stack.top()));
       }
     };
 
     struct semantic_analyzer {
 
-      struct production_t;
-      using productions_t = std::map<std::string, production_t>;
-
-      struct production_t {
-        std::function<bool (const productions_t& productions, const tokens_t&)> parse;
+      struct semantic_context {
+        std::map<std::string, semantic_def_stmt_t> functions; // XXX
+        std::stack<std::string> name_space; // tmp
       };
 
-      // stmt          : def_stmt*
-      // def_stmt      : LP __def IDENT lambda_stmt RP
-      // lambda_stmt:  : LP __lambda LP IDENT* RP LP lambda_body* RP RP
-      // lambda_body   : def_stmt | expr
-      // if_stmt       : LP __if expr expr expr RP
-      // fun_stmt      : LP IDENT expr* RP
-      // expr          : atom | fun_stmt | if_stmt
-      // atom          : BOOL | INT | DOUBLE | STRING | IDENT | lambda_stmt
-
-      struct program_stmt_t;
-      static inline productions_t productions = {
-        {
-          "stmt", {
-            [](const productions_t& productions, const tokens_t& tokens) {
-              while (productions.at("def_stmt").parse(productions, tokens)) { }
-              return true;
-            },
-          },
-        }
-      };
-
-      ast_t parse(ast_t ast) {
+      semantic_tree_t parse_program(semantic_tree_t semantic_tree, semantic_context& ctx) {
         DEBUG_LOGGER_TRACE_ULISP;
-        /*tokens_t tokens = tokens_orig;
-        productions.at("stmt").parse(productions, tokens);
-        if (!tokens.empty()) {
-          DEBUG_LOGGER_ULISP("WARN: unexpected: '%s'", show_token(tokens.front()).c_str());
-          DEBUG_LOGGER_ULISP("WARN: tokens.size(): %zd", tokens.size());
+        /*auto program_if = *ast;
+        if (!std::get_if<std::shared_ptr<token_list_t>>(&program_if))
+          throw std::runtime_error("semantic_analyzer: expected program");
+        auto program = std::get<std::shared_ptr<token_list_t>>(program_if);
+        for (const auto def_stmt : program->list) {
+          parse_def_stmt(def_stmt, ctx);
         }
-        return tokens;*/
+        return ast;*/
+        return semantic_tree;
+      };
+
+      /*ast_t parse_def_stmt(ast_t ast, semantic_context& ctx) {
+        DEBUG_LOGGER_TRACE_ULISP;
+        auto def_stmt_if = *ast;
+
+        if (!std::get_if<std::shared_ptr<token_list_t>>(&def_stmt_if))
+          throw std::runtime_error("semantic_analyzer: expected def_stmt");
+        std::vector<ast_t> args = std::get<std::shared_ptr<token_list_t>>(def_stmt_if)->list;
+
+        if (args.size() != 3)
+          throw std::runtime_error("semantic_analyzer: expected def_stmt");
+
+        if (!std::get_if<std::shared_ptr<token_ident_t>>(args[0].get())
+            || std::get<std::shared_ptr<token_ident_t>>(*args[0])->value != "__def")
+          throw std::runtime_error("semantic_analyzer: expected def_stmt");
+
+        if (!std::get_if<std::shared_ptr<token_ident_t>>(args[0].get()))
+          throw std::runtime_error("semantic_analyzer: expected def_stmt");
+        auto fun_name = std::get<std::shared_ptr<token_ident_t>>(*args[1])->value;
+
         return ast;
+      };*/
+
+      semantic_tree_t parse(syntax_tree_t syntax_tree) {
+        DEBUG_LOGGER_TRACE_ULISP;
+        semantic_context ctx;
+        // auto ret = parse_program(ast, ctx);
+        return {};
       }
     };
 
-    struct code_generator { };
+    struct code_generator {
+    };
 
     void test() {
       DEBUG_LOGGER_TRACE_ULISP;
       std::string code = R"LISP(
-        (def fib (lambda (x)
-          ((def fib (lambda (a b x) (if (greater? x 0) (fib b (+ a b) (- x 1)) (b))))
+        (__def fib (__lambda (x)
+          ((__def fib (__lambda (a b x) (__if (__greater? x 0) (fib b (+ a b) (- x 1)) (b))))
           (fib 1 1 x))))
       )LISP";
 
@@ -294,28 +324,28 @@ namespace lisp_utils {
         DEBUG_LOGGER_ULISP("code: '%s'", code.c_str());
       }
 
-      auto tokens = lexical_analyzer_t().parse(code);
+      auto lexemes = lexical_analyzer_t().parse(code);
 
       { // debug
         std::string str;
-        for (const auto& token : tokens) {
-          str += show_token(token) + " ";
+        for (const auto& lexeme : lexemes) {
+          str += show_lexeme(lexeme) + " ";
         }
-        DEBUG_LOGGER_ULISP("tokens: '\n%s'", str.c_str());
+        DEBUG_LOGGER_ULISP("lexemes: '\n%s'", str.c_str());
       }
 
-      ast_t syntax_tree = syntax_analyzer().parse(tokens);
+      auto syntax_tree = syntax_analyzer().parse(lexemes);
 
       { // debug
-        std::string str = show_ast(syntax_tree);
+        std::string str = show_syntax_tree(syntax_tree);
         DEBUG_LOGGER_ULISP("syntax_tree: '\n%s\n'", str.c_str());
       }
 
-      auto semantic_tree = semantic_analyzer().parse(syntax_tree);
+      // auto semantic_tree = semantic_analyzer().parse(syntax_tree);
 
       { // debug
-        std::string str = show_ast(semantic_tree);
-        DEBUG_LOGGER_ULISP("semantic_tree: '\n%s\n'", str.c_str());
+        // std::string str = show_ast(semantic_tree);
+        // DEBUG_LOGGER_ULISP("semantic_tree: '\n%s\n'", str.c_str());
       }
 
     }
