6a394cbdb36e6c9f3a5638fc6c6d63125dea2f7f
Alexander Myasnikov <myasnikov.alexander.s@gmail.com>
2019-09-22 17:32:38 +0300

    refactoring

--- yukoma.cpp
+++ yukoma.cpp
@@ -18,14 +18,41 @@ struct logger_indent_ulisp_t : logger_indent_t<logger_indent_ulisp_t> { };
 
 enum instruction_t : uint8_t {
   UNK,
-  LABEL,
-  STRING,
-  PUSH,
-  PUSHL,
-  ADD,
+  LOAD,
+  SAVE,
+  LLOAD,
+  LSAVE,
+  ALLOC,
+  FREE,
+  DROP,
+  JUMP,
   CALL,
   RET,
-  INT,
+  GETSP,
+  GETBP,
+
+  FUNC,
+  INTEGER,
+  FUNCTION,
+  VARIABLE,
+
+  // LOAD  : A    -> M[A]
+  // SAVE  : A, x ->             M[A] := x
+  // LLOAD : A    -> M[BP+A]     M[SP] := M[BP+A]
+  // LSAVE : A, V ->             M[BP+A] := V
+  //
+  // ALLOC : N -> A     A := MALLOC(N)
+  // FREE  : A ->       FREE(A)
+  //
+  // DROP : x    ->
+  //
+  // JUMP : b, A ->          if b THEN PC := A
+  //
+  // CALL : A -> PC            M[SP] <-> PC
+  // RET  : P0, ..., Pn ->     PC := RA; SP := RB
+  //
+  // GETBP :   -> BP     M[SP] := BP
+  // GETSP :   -> SP     M[SP] := SP
 };
 
 enum sys_call_t : uint8_t {
@@ -34,50 +61,137 @@ enum sys_call_t : uint8_t {
 
 
 
-struct stream_t {
-  std::deque<uint8_t> stream;
+struct reader_t {
 
-  void write(size_t pos, const void* data, size_t len) {
-    if (stream.size() < pos + len) {
-      stream.resize(pos + len);
-    }
-    std::copy(static_cast<const uint8_t*>(data), static_cast<const uint8_t*>(data) + len, stream.begin() + pos);
+  void read(void*& data, size_t len) {
   }
 
-  void read(size_t pos, void* data, size_t len) {
-    if (stream.size() < pos + len) {
-      throw std::runtime_error("could not read");
-    }
-    std::copy(stream.begin() + pos, stream.begin() + pos + len, static_cast<uint8_t*>(data));
-  }
-
-  void resize(size_t size) {
-    stream.resize(size);
-  }
-
-  size_t size() {
-    return stream.size();
-  }
-
-  std::string hex() {
-    std::stringstream ss;
-    ss << "{ ";
-    for (const auto b : stream) {
-      ss << std::hex << std::setfill('0') << std::setw(2) << static_cast<uint32_t>(b) << " ";
-    }
-    ss << "} " << std::dec << stream.size() << "s";
-    return ss.str();
+  void save_pos(size_t len) {
   }
 };
 
 
 
-// grammar:   command*
-// command:   IDENT ":" | IDENT IDENT*
+struct stream_t {
+  std::vector<uint8_t> __stream;
+  size_t               __pos;
+  reader_t             __reader;
+
+  stream_t() : __stream(100), __pos(0) { }
+  stream_t(const stream_t& stream) = default;
+
+  void* get_forward_pos(size_t len) {
+    if (__pos + len > __stream.size())
+      throw std::runtime_error("get_forward_pos: invalid pos");
+    return &__stream.at(__pos);
+  }
+
+  void save_forward_pos(size_t len) {
+    __pos += len;
+  }
+
+  void* get_backward_pos(size_t len) {
+    if (len > __pos)
+      throw std::runtime_error("get_backward_pos: invalid pos");
+    return &__stream.at(__pos);
+  }
+
+  void save_backward_pos(size_t len) {
+    __pos -= len;
+  }
+
+  std::string hex(size_t a = std::string::npos, size_t b = std::string::npos) {
+    if (a == std::string::npos && b == std::string::npos) {
+      a = 0;
+      b = __stream.size();
+    }
+    a = std::min(a, __stream.size());
+    b = a + std::min(b, __stream.size() - a);
+    std::stringstream ss;
+    ss << "{ ";
+    // for (const auto b : std::deque(stream.begin() + a, stream.begin() + b)) {
+    for (const auto b : __stream) {
+      ss << std::hex << std::setfill('0') << std::setw(2) << static_cast<uint32_t>(b) << " ";
+    }
+    ss << "} " << /*std::dec << (b - a) <<*/ "s";
+    return ss.str();
+  }
+};
+
+struct ttype_t {
+  std::string name;
+  size_t size;
+  std::function<void()> ctor;
+  std::function<void()> dtor;
+};
+
+struct type_t {
+  virtual std::string name() = 0;
+  virtual size_t size() = 0;
+  virtual void ctor(stream_t& stream, size_t pos = -1) = 0;
+  virtual void dtor(stream_t& stream, size_t pos = -1) = 0;
+  virtual void read(stream_t& stream, size_t pos = -1) = 0;
+  virtual void write(stream_t& stream, size_t pos = -1) = 0;
+};
+
+using type_sptr_t = std::shared_ptr<type_t>;
+
+struct type_int_t : type_t {
+  std::string name() override { return "i"; }
+  size_t size() override { return 4; }
+  void ctor(stream_t& stream, size_t pos = -1) override { DEBUG_LOGGER_ULISP("ctor"); }
+  void dtor(stream_t& stream, size_t pos = -1) override { DEBUG_LOGGER_ULISP("ctor"); }
+};
+
+
+struct stream_utils_t {
+
+  template <typename tvalue_t>
+  void static write_forward_pod(stream_t& stream, const tvalue_t& src) {
+    {
+      size_t len = sizeof(src);
+      void* dst = stream.get_forward_pos(sizeof(src));
+      memcpy(dst, &src, len);
+      stream.save_forward_pos(len);
+    }
+  }
+
+  template <typename tvalue_t>
+  void static read_forward_pod(stream_t& stream, tvalue_t& dst) {
+    {
+      size_t len = sizeof(dst);
+      void* src = stream.get_forward_pos(sizeof(dst));
+      memcpy(&dst, src, len);
+      stream.save_forward_pos(len);
+    }
+  }
+
+  size_t static write_varint(size_t pos, stream_t& stream, uint64_t value) {
+    size_t ret{};
+    if (value < 252) {
+      auto val = static_cast<uint8_t>(value);
+      // ret += stream.write(pos, &val, sizeof(val));
+    } else {
+      throw std::runtime_error("write_varint: todo");
+    }
+    return ret;
+  }
+};
+
+
+
+struct fun_t {
+  std::string name;
+  ttype_t type_ret;
+  std::vector<std::pair<ttype_t, std::string>> args;
+  std::vector<std::pair<ttype_t, std::string>> local_vars;
+};
+
+
 
 struct analyzer_t {
   struct lexeme_empty_t   { };
-  struct lexeme_integer_t { int         value; };
+  struct lexeme_integer_t { int32_t     value; };
   struct lexeme_ident_t   { std::string value; };
   struct lexeme_string_t  { std::string value; };
 
@@ -139,78 +253,93 @@ struct analyzer_t {
 
     stream_t data;
     std::map<std::string, uint32_t> labels;
-    std::vector<std::pair<std::string, uint32_t>> idents;
+    std::shared_ptr<fun_t> fun_curr;
+
+    std::vector<ttype_t> types = {
+      {
+        "int",
+        4,
+        []() { },
+        []() { },
+      },
+    };
 
     {
       size_t pos = 0;
-      for (size_t i{}; i < lexemes.size(); ++i) {
-        const auto& instruction = std::get<lexeme_ident_t>(lexemes.at(i)).value;
-        if (instruction == "LABEL") {
-          const auto& label = std::get<lexeme_ident_t>(lexemes.at(++i)).value;
-          DEBUG_LOGGER_ULISP("LABEL %s", label.c_str());
-          labels[label] = pos;
-        } else if (instruction == ".string") {
-          const auto& value = std::get<lexeme_string_t>(lexemes.at(++i)).value;
-          DEBUG_LOGGER_ULISP(".string \"%s\"", value.c_str());
-          {
-            data.write(pos, value.data(), value.size() + 1);
-            pos += sizeof(value.size() + 1);
-          }
-        } else if (instruction == "PUSH") {
-          const auto& value = std::get<lexeme_integer_t>(lexemes.at(++i)).value;
-          DEBUG_LOGGER_ULISP("PUSH %d", value);
-          {
-            auto cmd = static_cast<uint8_t>(PUSH);
-            data.write(pos, &cmd, sizeof(cmd));
-            pos += sizeof(cmd);
-            auto val = static_cast<uint32_t>(value);
-            data.write(pos, &val, sizeof(val));
-            pos += sizeof(val);
-          }
-        } else if (instruction == "PUSHL") {
-          const auto& value = std::get<lexeme_ident_t>(lexemes.at(++i)).value;
-          DEBUG_LOGGER_ULISP("PUSHL %s", value.c_str());
-          {
-            auto cmd = static_cast<uint8_t>(PUSHL);
-            data.write(pos, &cmd, sizeof(cmd));
-            pos += sizeof(cmd);
-            idents.push_back({value, pos});
-            auto val = static_cast<uint32_t>(0);
-            data.write(pos, &val, sizeof(val));
-            pos += sizeof(val);
-          }
-        } else if (instruction == "INT") {
-          DEBUG_LOGGER_ULISP("INT");
-          {
-            auto cmd = static_cast<uint8_t>(INT);
-            data.write(pos, &cmd, sizeof(cmd));
-            pos += sizeof(cmd);
-          }
-        } else if (instruction == "CALL") {
-          DEBUG_LOGGER_ULISP("CALL");
-          {
-            auto cmd = static_cast<uint8_t>(CALL);
-            data.write(pos, &cmd, sizeof(cmd));
-            pos += sizeof(cmd);
-          }
-        } else if (instruction == "RET") {
-          DEBUG_LOGGER_ULISP("RET");
-          {
-            auto cmd = static_cast<uint8_t>(RET);
-            data.write(pos, &cmd, sizeof(cmd));
-            pos += sizeof(cmd);
-          }
-        } else {
-          throw std::runtime_error("unknown instruction 2");
+      size_t i = 0;
+
+      while (i < lexemes.size()) {
+        std::string lexeme_curr;
+        lexeme_curr = std::get<lexeme_ident_t>(lexemes.at(i++)).value;
+        if (lexeme_curr!= "FUNC") throw std::runtime_error("expected FUNC");
+
+        const auto& fname = std::get<lexeme_ident_t>(lexemes.at(i++)).value;
+        // const auto& ret_type = std::get<lexeme_ident_t>(lexemes.at(i++)).value;
+        DEBUG_LOGGER_ULISP("FUNC %s", fname.c_str());
+
+        lexeme_curr = std::get<lexeme_ident_t>(lexemes.at(i++)).value;
+
+        DEBUG_LOGGER_ULISP("ARG");
+
+        while (true) {
+          if (std::get<lexeme_ident_t>(lexemes.at(i)).value == "VAR") break;
+
+          const auto& arg  = std::get<lexeme_ident_t>(lexemes.at(i++)).value;
+          const auto& type = std::get<lexeme_ident_t>(lexemes.at(i++)).value;
+          DEBUG_LOGGER_ULISP("  %s %s", arg.c_str(), type.c_str());
         }
+
+        lexeme_curr = std::get<lexeme_ident_t>(lexemes.at(i++)).value;
+
+        DEBUG_LOGGER_ULISP("VAR");
+
+        while (true) {
+          if (std::get<lexeme_ident_t>(lexemes.at(i)).value == "BEGIN") break;
+
+          const auto& arg  = std::get<lexeme_ident_t>(lexemes.at(i++)).value;
+          const auto& type = std::get<lexeme_ident_t>(lexemes.at(i++)).value;
+          DEBUG_LOGGER_ULISP("  %s %s", arg.c_str(), type.c_str());
+        }
+
+        lexeme_curr = std::get<lexeme_ident_t>(lexemes.at(i++)).value;
+        DEBUG_LOGGER_ULISP("BEGIN");
+
+        while (lexeme_curr != "END") {
+          const auto& instruction = std::get<lexeme_ident_t>(lexemes.at(i++)).value;
+          if (instruction == "INTEGER") {
+            const auto& value = std::get<lexeme_integer_t>(lexemes.at(i++)).value;
+            stream_utils_t::write_forward_pod(data, static_cast<uint8_t>(INTEGER));
+            stream_utils_t::write_forward_pod(data, static_cast<int32_t>(value));
+            DEBUG_LOGGER_ULISP("  INTEGER %d", value);
+          } else if (instruction == "FUNCTION") {
+            const auto& value = std::get<lexeme_ident_t>(lexemes.at(i++)).value;
+            DEBUG_LOGGER_ULISP("  FUNCTION %s", value.c_str());
+          } else if (instruction == "VARIABLE") {
+            const auto& value = std::get<lexeme_ident_t>(lexemes.at(i++)).value;
+            DEBUG_LOGGER_ULISP("  VARIABLE %s", value.c_str());
+          } else if (instruction == "CALL") {
+            DEBUG_LOGGER_ULISP("  CALL");
+          } else if (instruction == "ADDI") {
+            DEBUG_LOGGER_ULISP("  ADDI");
+          } else {
+            throw std::runtime_error("unknown instruction: " + instruction);
+          }
+          lexeme_curr = std::get<lexeme_ident_t>(lexemes.at(i)).value.c_str();
+        }
+
+        lexeme_curr = std::get<lexeme_ident_t>(lexemes.at(i++)).value.c_str();
+        if (lexeme_curr!= "END") throw std::runtime_error("expected END");
+        DEBUG_LOGGER_ULISP("END");
+        DEBUG_LOGGER_ULISP("");
+
       }
     }
 
     {
-      for (const auto& [label, pos] : idents) {
+      /*for (const auto& [label, pos] : idents) {
         auto val = static_cast<uint32_t>(labels[label]);
         data.write(pos, &val, sizeof(val));
-      }
+      }*/
     }
 
     return data;
@@ -233,150 +362,132 @@ struct interpreter_t {
     uint32_t ip = 0;
     uint32_t bp = 0;
     uint32_t sp = 0;
+    bool run = true;
 
-    stack.write(sp, &ip, sizeof(ip));
-    sp += sizeof(ip);
-    stack.write(sp, &bp, sizeof(bp));
-    sp += sizeof(bp);
     bp = sp;
 
     DEBUG_LOGGER_ULISP("data: %s", data.hex().c_str());
 
-    while (true) {
+    data.__pos = {}; // XXX
+
+    while (run) {
       DEBUG_LOGGER_ULISP("");
-      DEBUG_LOGGER_ULISP("stack: %s", stack.hex().c_str());
+      DEBUG_LOGGER_ULISP("data: ... %s ...", data.hex(ip, ip + 100).c_str());
+      DEBUG_LOGGER_ULISP("stack: %s ...", stack.hex(0, sp).c_str());
       DEBUG_LOGGER_ULISP("ip: %x", ip);
       DEBUG_LOGGER_ULISP("bp: %x", bp);
       DEBUG_LOGGER_ULISP("sp: %x", sp);
 
       uint8_t cmd;
-      data.read(ip, &cmd, sizeof(cmd));
+      stream_utils_t::read_forward_pod(data, cmd);
       DEBUG_LOGGER_ULISP("cmd: %hhx", cmd);
       ip += sizeof(cmd);
 
       switch(cmd) {
-        case PUSHL:
-        case PUSH: {
-          uint32_t op1;
-          data.read(ip, &op1, sizeof(op1));
-          ip += sizeof(op1);
-          stack.write(sp, &op1, sizeof(op1));
-          sp += sizeof(op1);
-          DEBUG_LOGGER_ULISP("PUSHX: %x", op1);
+        case SAVE: {
+          DEBUG_LOGGER_ULISP("SAVE:");
+          uint32_t op1{};
+          uint32_t op2{};
+          // sp -= stack.read(sp - sizeof(op1), &op1, sizeof(op1));
+          // sp -= stack.read(sp - sizeof(op2), &op2, sizeof(op2));
+          DEBUG_LOGGER_ULISP("  op1: %x", op1);
+          DEBUG_LOGGER_ULISP("  op2: %x", op2);
+          // SAVE  : A, x ->             M[A] := x
           break;
         }
-        case CALL: {
-          uint32_t op1;
-          stack.read(sp - sizeof(op1), &op1, sizeof(op1));
-
-          stack.write(sp, &ip, sizeof(ip));
-          sp += sizeof(ip);
-          ip = op1;
-
-          stack.write(sp, &bp, sizeof(bp));
-          sp += sizeof(bp);
-          bp = sp;
-          DEBUG_LOGGER_ULISP("CALL");
+        case INTEGER: {
+          DEBUG_LOGGER_ULISP("INTEGER:");
+          int32_t op1{};
+          stream_utils_t::read_forward_pod(data, op1);
+          stream_utils_t::write_forward_pod(stack, static_cast<int32_t>(op1));
+          stream_utils_t::write_forward_pod(stack, static_cast<uint8_t>(INTEGER));
+          DEBUG_LOGGER_ULISP("  op1: %d   %x", op1, op1);
           break;
         }
         case RET: {
-          uint32_t ip_orig;
-          uint32_t bp_orig;
-          stack.resize(bp);
-          sp = bp;
-          stack.read(sp - sizeof(bp_orig), &bp_orig, sizeof(bp_orig));
-          sp -= sizeof(bp_orig);
-          stack.read(sp - sizeof(ip_orig), &ip_orig, sizeof(ip_orig));
-          sp -= sizeof(ip_orig);
-          stack.resize(sp);
-          ip = ip_orig;
-          bp = bp_orig;
-          DEBUG_LOGGER_ULISP("RET");
+          DEBUG_LOGGER_ULISP("RET:");
+          if (!bp) run = false;
+          // uint32_t op1{};
+          // ip += data.read(ip, &op1, sizeof(op1));
+          // sp += stack.write(sp, &op1, sizeof(op1));
+          // DEBUG_LOGGER_ULISP("  op1: %x", op1);
           break;
         }
-        case INT: {
-          uint32_t op1;
-          stack.read(sp - sizeof(op1), &op1, sizeof(op1));
-          sp -= sizeof(op1);
-
-          switch (op1) {
-            case PRINT: {
-              uint32_t op1;
-              stack.read(sp - sizeof(op1), &op1, sizeof(op1));
-              sp -= sizeof(op1);
-              out.write(out.size(), &op1, sizeof(op1));
-              break;
-            }
-            default : {
-              break;
-            }
-          }
-          DEBUG_LOGGER_ULISP("INT");
-          break;
-        }
-        /*case ADD: {
-          uint64_t op1;
-          uint64_t op2;
-          stack.read(rsp - sizeof(op2), &op2, sizeof(op2));
-          rsp -= sizeof(op2);
-          stack.read(rsp - sizeof(op1), &op1, sizeof(op1));
-          rsp -= sizeof(op1);
-          DEBUG_LOGGER_ULISP("ADD: %x %x", op1, op2);
-          uint64_t ret = op1 + op2;
-          stack.write(rsp, &ret, sizeof(ret));
-          break;
-        }*/
-        default: throw std::runtime_error("unknown instruction 1");
+        default: throw std::runtime_error("unknown instruction: " + std::to_string(cmd));
       }
-
-      if (!ip) break;
     }
   }
 };
 
 
+/*
+
+; int
+; double
+; char
+; pointer ...
+; struct ...
+
+; type person_t struct
+;   id: int
+;   age: int
+; end
+; 
+; type pint_t pointer int
+; 
+; type array_t struct
+;   data: pint_t
+;   size: int
+; end
+; 
+; type string_t struct
+;   data :pint_t
+;   size :int
+;   cap  :int
+; end
+
+function sum :int
+arg
+  x :int
+  y :int
+var
+  ret :int
+begin
+  LLOAD x
+  LLOAD y
+  ADDI
+end
+
+*/
+
+// person.name -> &person + person_offset(name)
 
 int main() {
   std::string code = R"ASM(
-    LABEL main
-      PUSH 5
-      PUSH 2
-      PUSHL sum
+    FUNC main
+    ARG
+    VAR
+    BEGIN
+      INTEGER 10
+      INTEGER 12
+      FUNCTION sum
       CALL
-      PUSHL test_print
+      INTEGER 14
+      FUNCTION sum
       CALL
-      RET
+    END
 
-    LABEL hello_str
-      .string "Hello, World!"
-
-    LABEL mul
-      PUSH 21
-      INT
-      RET
-
-    LABEL sum
-      PUSH 20
-      INT
-      RET
-
-    LABEL test_print
-      PUSH 72              ; 'H'
-      PUSHL __print_char
-      CALL
-      PUSH 105             ; 'i'
-      PUSHL __print_char
-      CALL
-      PUSH 33              ; '!'
-      PUSHL __print_char
-      CALL
-      RET
-
-    LABEL __print_char
-      PUSH 0                ; syscall PRINT
-      INT
-      RET
-
+    FUNC sum
+    ARG
+      x int
+      y int
+    VAR
+      ret int
+    BEGIN
+      VARIABLE x
+      VARIABLE y
+      ADDI
+    END
     )ASM";
 
   auto data = analyzer_t::parse(code);
